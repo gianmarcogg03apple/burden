9cb81962a739005afbac5325ba7ab5a4da62fdda
gianmarcogg03apple <113519788+gianmarcogg03apple@users.noreply.github.com>
2022-10-20 12:36:18 +0200

    Initial Commit

---
+++ Burden/BurdenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurdenApp.swift
+//  Burden
+//
+//  Created by Gianmarco on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BurdenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
