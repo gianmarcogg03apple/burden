07bf18ace451b8c272c308690b1020a6cfb4086b
Sarah <sndenbe22@fed.idserve.net>
2022-10-25 06:59:55 +0200

    Update dataset emotion

--- Burden/Model/OnePlayerGameModel.swift
+++ Burden/Model/OnePlayerGameModel.swift
@@ -16,25 +16,43 @@ struct OnePlayerGame{
 }
 
 struct Spartito {
-    var aPlay : [EmotionShape]
+    var aPlay : [Emotion]
 }
 
 struct Emotion {
-    var spartito: Spartito
     var colorName: String
     var emotionTitle: EmotionName
+    var emotionShape: EmotionShape
 }
 
 struct EmotionShape {
     var shapePictureName: String
+    var shapeColor: ShapeColor
     var shapeSound: String
     var shapeVibration: String
     var isTouched: Bool
     var isVisible: Bool
-    var positionOnThePlayground: Int
+    var isVibrating: Bool
+    var isMakingMusic: Bool
+    var shapeOpacity: Double
+    var numberOfRadiantCircle: Int
+    var radiantCirclePictureBundle: [RadiantCircle]
+    var positionOnThePlayground: (Int,Int)
+}
+
+struct PlaygroundGrid {
+    var aGrid: [(Int,Int)]
+}
+
+enum RadiantCircle {
+    case Tiny,Small,Medium,Large
 }
 
 enum EmotionName {
     case Sad,Shocked,Anxious,Strong,Happy,Worried,Angry,Disgusted,Remorse,Disapointed,Afraid,Submission,Loved,Optimistic,Aggressiveness,Contempt
     
+}
+
+enum ShapeColor {
+    case Purple,Blue,DarkGreen,Green,LightGreen,Yellow,Orange,Pink
 }
