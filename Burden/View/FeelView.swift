d055d737ccf5f2c7aafbf80a4dd9db697a6e279a
giroboa <113997953+giroboa@users.noreply.github.com>
2022-10-26 10:09:23 +0200

    Updated FeelView with emotion splatter assets imported

--- Burden/View/FeelView.swift
+++ Burden/View/FeelView.swift
@@ -23,15 +23,110 @@ struct FeelView: View {
 // Mood selector area with shapes around
 struct FeelSelector: View {
     var body: some View {
-        VStack{
-            List {
-                Picker(selection: .constant(1), label: Text("--")) {
-                    Text("Angry").tag(1)
-                    Text("Sad").tag(2)
+        VStack(alignment:.leading) {
+            
+            //Random shapes at the top, not intended to be here, so they're commented out
+            
+            /*HStack{
+                Image(systemName: "triangle.fill")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .foregroundColor(Color(hue: 0.524, saturation: 0.743, brightness: 0.946))
+                    .frame(width: 50.0, height: 50.0)
+                Spacer()
+                    .frame(width:50)
+                
+                
+                Image(systemName: "square.fill")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .foregroundColor(Color(hue: 0.302, saturation: 1.0, brightness: 0.729))
+                    .frame(width: 50.0,
+                           height: 50.0)
+                Spacer()
+                    .frame(width:50)
+                
+                Image(systemName: "circle.fill")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .foregroundColor(Color(hue: 0.283, saturation: 0.876, brightness: 0.989))
+                    .frame(width: 50.0,
+                           height: 50.0)
+                Spacer()
+                    .frame(width:50)
+                
+                
+                Image(systemName: "circle.fill")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .foregroundColor(Color(hue: 0.217, saturation: 0.736, brightness: 0.931))
+                    .frame(width: 50.0,
+                           height: 50.0)
+            }
+            
+            Spacer()
+                .frame(height:60)*/
+            
+            VStack(alignment:.leading) {
+                GroupBox(){
+                    HStack {
+                        Image("es-blue")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-orange")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-gray")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                    }
+                    
+                    Spacer()
+                        .frame(height:30)
+                    
+                    HStack {
+                        Image("es-cyan")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-red")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-lime")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                    }
+                    
+                    Spacer()
+                        .frame(height:30)
+                    
+                    HStack {
+                        Image("es-purple")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-yellow")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                        Spacer()
+                            .frame(width:30)
+                        Image("es-darkcyan")
+                            .resizable()
+                            .frame(width: 90 , height: 90)
+                    }
                 }
             }
-        }.padding(.bottom)
+            Spacer()
+        }
     }
+}
+
+func Back(){
+   //Function to go back
 }
 
 // Header part with "back" button
@@ -46,7 +141,7 @@ struct FeelHeader: View {
                 
                 HStack{
                     Button("Player Mode") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        Back()
                     }
                     Spacer()
                 }.padding(.horizontal)
