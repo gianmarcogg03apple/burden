afe88263405ca5c069b100376e1ae279db522b0c
giroboa <113997953+giroboa@users.noreply.github.com>
2022-10-26 12:51:28 +0200

    Updated the FeelView with the final design

--- Burden/View/FeelView.swift
+++ Burden/View/FeelView.swift
@@ -4,155 +4,232 @@
 //
 //  Created by Gianmarco on 24/10/22.
 //
-//  I'm sorry, I can't do much, SwiftUI sucks.
-//
 
 import SwiftUI
 
 struct FeelView: View {
     var body: some View {
         ZStack{
-            VStack{
-                FeelHeader()
-                FeelSelector()
-            }
-        }
-    }
-}
-
-// Mood selector area with shapes around
-struct FeelSelector: View {
-    var body: some View {
-        VStack(alignment:.leading) {
+            Color("BackgroundColorHeader")
             
-            //Random shapes at the top, not intended to be here, so they're commented out
-            
-            /*HStack{
-                Image(systemName: "triangle.fill")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                    .foregroundColor(Color(hue: 0.524, saturation: 0.743, brightness: 0.946))
-                    .frame(width: 50.0, height: 50.0)
-                Spacer()
-                    .frame(width:50)
-                
-                
-                Image(systemName: "square.fill")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                    .foregroundColor(Color(hue: 0.302, saturation: 1.0, brightness: 0.729))
-                    .frame(width: 50.0,
-                           height: 50.0)
-                Spacer()
-                    .frame(width:50)
-                
-                Image(systemName: "circle.fill")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                    .foregroundColor(Color(hue: 0.283, saturation: 0.876, brightness: 0.989))
-                    .frame(width: 50.0,
-                           height: 50.0)
-                Spacer()
-                    .frame(width:50)
-                
-                
-                Image(systemName: "circle.fill")
-                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
-                    .foregroundColor(Color(hue: 0.217, saturation: 0.736, brightness: 0.931))
-                    .frame(width: 50.0,
-                           height: 50.0)
-            }
-            
-            Spacer()
-                .frame(height:60)*/
-            
+        
             VStack(alignment:.leading) {
-                GroupBox(){
-                    HStack {
-                        Image("es-blue")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-orange")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-gray")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                    }
+                
+                Button(action: Back) {
+                    Text ("Player Mode")
+                }.padding(.horizontal, 10)
+                
+                    .frame(height:15)
+                Text ("HOW DO YOU FEEL?")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                    .multilineTextAlignment(.leading)
+                    .frame(height:25)
+                    .padding(.horizontal, 10)
+
+                
+                HStack{
+                    Image(systemName: "triangle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.524, saturation: 0.743, brightness: 0.946))
+                        .frame(width: 50.0, height: 50.0)
+                        .frame(width:50)
+                        .padding()
                     
-                    Spacer()
-                        .frame(height:30)
                     
-                    HStack {
-                        Image("es-cyan")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-red")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-lime")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                    }
+                    Image(systemName: "square.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.302, saturation: 1.0, brightness: 0.729))
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .frame(width:50)
+                        .padding()
                     
-                    Spacer()
-                        .frame(height:30)
+                    Image(systemName: "circle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.283, saturation: 0.876, brightness: 0.989))
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .frame(width:50)
+                        .padding()
                     
-                    HStack {
-                        Image("es-purple")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-yellow")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                        Spacer()
-                            .frame(width:30)
-                        Image("es-darkcyan")
-                            .resizable()
-                            .frame(width: 90 , height: 90)
-                    }
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.217, saturation: 0.736, brightness: 0.931))
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .padding()
                 }
+                .padding(.bottom, 20)
+                
+                HStack{
+                    
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Sad")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.cyan)
+                    .cornerRadius(10)
+                    .padding()
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Warned")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color(hue: 0.302, saturation: 1.0, brightness: 0.729))
+                    .cornerRadius(10)
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Happy")
+                            .padding()
+                            .foregroundColor(Color.black)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                   
+                }
+                .padding(.bottom, 20)
+                
+                HStack{
+                    
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Anxious")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color(hue: 0.283, saturation: 0.876, brightness: 0.850))
+                    .cornerRadius(10)
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Angry")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    .padding()
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Worried")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+                   
+                }
+                .padding(.bottom, 50)
+                
+                HStack{
+                    
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Impatient")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.purple)
+                    .cornerRadius(10)
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Powerful")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    Button(action: {
+                        print("Button tapped!")
+                    }, label: {
+                        Text("Neutral")
+                            .padding()
+                            .foregroundColor(Color.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                   
+                }
+                .padding(.bottom, 20)
+                
+                HStack{
+                    Image(systemName: "triangle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.839, saturation: 0.511, brightness: 0.897))
+                        .frame(width: 50.0, height: 50.0)
+                        .frame(width:50)
+                        .padding()
+                    
+                    
+                    Image(systemName: "square.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color.pink)
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .frame(width:50)
+                        .padding()
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.128, saturation: 0.943, brightness: 1.0))
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .frame(width:50)
+                        .padding()
+                    
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .foregroundColor(Color(hue: 0.158, saturation: 0.97, brightness: 0.996))
+                        .frame(width: 50.0,
+                               height: 50.0)
+                        .padding()
+                }
+                .padding(.top, 50)
+                Button(action: {
+                    print("Button tapped!")
+                }, label: {
+                    Text("Confirm")
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                .frame(maxWidth: .infinity)
+                .background(Color.black)
+                .cornerRadius(10)
+                .padding()
+                
             }
-            Spacer()
+            .frame(width: 370)
         }
+        .ignoresSafeArea()
+
     }
 }
 
 func Back(){
    //Function to go back
-}
-
-// Header part with "back" button
-struct FeelHeader: View {
-    var body: some View {
-        ZStack{
-            
-            Rectangle()
-                .foregroundColor(Color("BackgroundColorHeader"))
-                .frame(height: 200)
-            VStack{
-                
-                HStack{
-                    Button("Player Mode") {
-                        Back()
-                    }
-                    Spacer()
-                }.padding(.horizontal)
-                HStack{
-                    Text("HOW DO YOU FEEL?")
-                        .padding(15)
-                    Spacer()
-                }.padding(.horizontal)
-            }
-        }
-    }
 }
 
 struct FeelView_Previews: PreviewProvider {
